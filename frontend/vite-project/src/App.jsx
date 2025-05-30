5dc9a7ed89bef3457bd58b3062efd40099289a75
Neha Karmakar <nkca1998@gmail.com>
2025-05-30 18:28:15 -0300

    Product management update

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,13 +1,22 @@
-
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import Products from "./pages/Products/Products";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
   return (
     <>
-      <h1>Hello World..</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
